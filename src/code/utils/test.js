e22e04ea27f1fa7045f9bf6b5b4d875b799fb12b
Caloyko <pauline.lubet@gmail.com>
2026-01-08 11:56:47 +0100

    Updates

--- src/code/utils/test.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-try {
-  const fileContent = fs.readFileSync('../../data/query-results/query-9.1.csv', 'utf8');
-  console.log(fileContent);
-} catch (err) {
-  console.error('Erreur lors de la lecture du fichier :', err);
-}
